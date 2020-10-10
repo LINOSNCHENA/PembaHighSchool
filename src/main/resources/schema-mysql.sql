d4ce260751b439dc398d47ca540cc400fb04baa2
LINOS NCHENA <linosnchena@gmail.com>
2020-10-10 12:43:04 +0200

    reduced  sample record to five records

--- src/main/resources/schema-mysql.sql
+++ src/main/resources/schema-mysql.sql
@@ -2,7 +2,6 @@ use presly;
 DROP TABLE IF EXISTS teachers;
 CREATE TABLE teachers(id serial PRIMARY KEY, name VARCHAR(255), code integer);
 
-
 DROP TABLE IF EXISTS students;
 CREATE TABLE students(id serial PRIMARY KEY, name VARCHAR(255), code integer);
 
@@ -13,7 +12,6 @@ CREATE TABLE courses(id serial PRIMARY KEY, name VARCHAR(255), code integer);
 INSERT INTO courses(name, code) VALUES('INSY118 MySQL', 3011);
 INSERT INTO courses(name, code) VALUES('CoronaVirus', 3013);
 INSERT INTO courses(name, code) VALUES('INSY308', 2012);
-INSERT INTO courses(name, code) VALUES('INSY336', 1013);
 INSERT INTO courses(name, code) VALUES('COSC111', 5016);
 INSERT INTO courses(name, code) VALUES('INSY336 MySQL', 2013);
 
@@ -22,21 +20,13 @@ INSERT INTO teachers(name, code) VALUES('Chilube MySQL', 43201.00);
 INSERT INTO teachers(name, code) VALUES('Mwetwa', 17590.01);
 INSERT INTO teachers(name, code) VALUES('Coruna Virus', 12800.09);
 INSERT INTO teachers(name, code) VALUES('Andreevna', 17480.01);
-INSERT INTO teachers(name, code) VALUES('Kirpichnikov', 39710.04);
-INSERT INTO teachers(name, code) VALUES('Dogmar Grga', 85500.08);
-INSERT INTO teachers(name, code) VALUES('Korova Virus', 4640.02);
 INSERT INTO teachers(name, code) VALUES('Ncube MySQL', 36710.02);
 
 
 INSERT INTO students(name, code) VALUES('Korona Virus MySQL', 1983);
 INSERT INTO students(name, code) VALUES('Kachana', 1984);
 INSERT INTO students(name, code) VALUES('Corana Virus', 1989);
-INSERT INTO students(name, code) VALUES('Diana', 1988);
-INSERT INTO students(name, code) VALUES('Kelvin', 1990);
-INSERT INTO students(name, code) VALUES('Kristine', 1991);
-INSERT INTO students(name, code) VALUES('Biggie', 1991);
 INSERT INTO students(name, code) VALUES('Kachana', 1990);
-INSERT INTO students(name, code) VALUES('Justina', 1994);
 INSERT INTO students(name, code) VALUES('Marvin MySQL', 1993);
 
 select * from teachers, courses, students
